9547918030deace80887f03a0d7945250c8aa4a0
ryota.doi <ryota.doi.1102@gmail.com>
2021-11-01 17:26:09 +0900

    :tada: begin a project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const TopPage: NextPage = () => (
+  <>
+    <header>keisan</header>
+    <main>Welcome to Keisan!</main>
+    <footer>copyright</footer>
+  </>
+);
+
+export default TopPage;
