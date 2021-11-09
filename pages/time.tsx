62b3d594f74491674172c10bfd9862b582c51fd8
Shalin Okada <shalin.okada@wnd.co.jp>
2021-11-09 17:12:21 +0900

    :recycle: Summarize state for hooks

--- pages/time.tsx
+++ pages/time.tsx
@@ -1,108 +1,64 @@
-import { add, differenceInDays, format, sub } from 'date-fns';
+import useCalculateTime from 'hooks/useCalculateTime';
 import { NextPage } from 'next';
-import { useState } from 'react';
 
 const TimePage: NextPage = () => {
-  const [targetHour, setTargetHour] = useState<number | ''>(0);
-  const [targetMinute, setTargetMinute] = useState<number | ''>(0);
-  const [targetSecond, setTargetSecond] = useState<number | ''>(0);
-  const [elapsedHour, setElapsedHour] = useState<number | ''>(0);
-  const [elapsedMinute, setElapsedMinute] = useState<number | ''>(0);
-  const [elapsedSecond, setElapsedSecond] = useState<number | ''>(0);
-  const [isBefore, setIsBefore] = useState(true);
-  const [time, setTime] = useState<Date | null>(null);
-  const [difference, setDifference] = useState(0);
+  const {
+    targetHour,
+    targetMinute,
+    targetSecond,
+    elapsedHour,
+    elapsedMinute,
+    elapsedSecond,
+    isBefore,
+    time,
+    difference,
+    changeTargetHour,
+    changeTargetMinute,
+    changeTargetSecond,
+    changeElapsedHour,
+    changeElapsedMinute,
+    changeElapsedSecond,
+    changeIsBefore,
+    calculate,
+    reset,
+  } = useCalculateTime();
 
   const handleChangeTargetHour: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value;
-    if (value === '') {
-      setTargetHour(value);
-    } else {
-      setTargetHour(Number.parseInt(value));
-    }
+    changeTargetHour(value);
   };
   const handleChangeTargetMinute: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value;
-    if (value === '') {
-      setTargetMinute(value);
-    } else {
-      setTargetMinute(Number.parseInt(value));
-    }
+    changeTargetMinute(value);
   };
   const handleChangeTargetSecond: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value;
-    if (value === '') {
-      setTargetSecond(value);
-    } else {
-      setTargetSecond(Number.parseInt(value));
-    }
+    changeTargetSecond(value);
   };
   const handleChangeElapsedHour: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value;
-    if (value === '') {
-      setElapsedHour(value);
-    } else {
-      setElapsedHour(Number.parseInt(value));
-    }
+    changeElapsedHour(value);
   };
   const handleChangeElapsedMinute: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value;
-    if (value === '') {
-      setElapsedMinute(value);
-    } else {
-      setElapsedMinute(Number.parseInt(value));
-    }
+    changeElapsedMinute(value);
   };
   const handleChangeElapsedSecond: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value;
-    if (value === '') {
-      setElapsedSecond(value);
-    } else {
-      setElapsedSecond(Number.parseInt(value));
-    }
+    changeElapsedSecond(value);
   };
   const handleChangeIsBefore: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const value = event.target.value;
-    setIsBefore(value === '1');
+    changeIsBefore(value);
   };
 
   const handleClickSubmitButton: React.MouseEventHandler<HTMLInputElement> = (event) => {
     event.preventDefault();
-    const targetDate = new Date();
-    const hours = targetHour === '' ? 0 : targetHour;
-    const minutes = targetMinute === '' ? 0 : targetMinute;
-    const seconds = targetSecond === '' ? 0 : targetSecond;
-    targetDate.setHours(hours);
-    targetDate.setMinutes(minutes);
-    targetDate.setSeconds(seconds);
-    let elapsedDate: Date;
-    if (isBefore) {
-      elapsedDate = sub(targetDate, {
-        hours: elapsedHour === '' ? 0 : elapsedHour,
-        minutes: elapsedMinute === '' ? 0 : elapsedMinute,
-        seconds: elapsedSecond === '' ? 0 : elapsedSecond,
-      });
-    } else {
-      elapsedDate = add(targetDate, {
-        hours: elapsedHour === '' ? 0 : elapsedHour,
-        minutes: elapsedMinute === '' ? 0 : elapsedMinute,
-        seconds: elapsedSecond === '' ? 0 : elapsedSecond,
-      });
-    }
-    setTime(elapsedDate);
-    setDifference(differenceInDays(elapsedDate, targetDate));
+    calculate();
   };
   const handleClickResetButton: React.MouseEventHandler<HTMLInputElement> = (event) => {
     event.preventDefault();
-    setTargetHour(0);
-    setTargetMinute(0);
-    setTargetSecond(0);
-    setElapsedHour(0);
-    setElapsedMinute(0);
-    setElapsedSecond(0);
-    setIsBefore(true);
-    setTime(null);
-    setDifference(0);
+    reset();
   };
 
   return (
@@ -136,7 +92,7 @@ const TimePage: NextPage = () => {
         <input type="reset" value="クリア" onClick={handleClickResetButton} />
         <div>
           <output>
-            {difference}日{time && format(time, 'HH:mm:ss')}
+            {difference}日{time}
           </output>
         </div>
       </form>
